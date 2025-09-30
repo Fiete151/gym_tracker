bea030774e44088c6c756cb58fd91d9f24de26ac
Fiete151 <fiete2009@mail-de>
2025-09-30 20:19:48 +0200

    Anmeldung komplett

--- gym_tracker.py
+++ gym_tracker.py
@@ -5,8 +5,6 @@ import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime
 
-st.json(st.secrets["GOOGLE_CREDS"])
-
 
 service_account_info = json.loads(st.secrets["GOOGLE_CREDS"])
 
@@ -26,16 +24,26 @@ sh = gc.open("gym_tracker_data").sheet1  # Name deines Sheets
 phase = 0
 
 # --- App-Titel ---
-st.title("Mini Gym Tracker 💪, Updated, second time")
-
-
+st.title("Mini Gym Tracker 💪")
 
 if phase == 0:
     username = st.text_input("Benutzername")
-    password = st.text_input("Passwort")
+    password = st.text_input("Passwort", type="password")
     st.subheader("Anmelden/Registrieren")
+
     if st.button("Anmelden"):
-        st.subheader("Anmelden/Registrieren")
+        users = sh.col_values(1)  # Annahme: Benutzernamen in Spalte 1
+        passwords = sh.col_values(2)  # Passwörter in Spalte 2
+        if username in users:
+            index = users.index(username)
+            if password == passwords[index]:
+                st.success(f"Willkommen zurück, {username}!")
+                phase = 1  # Phase wechseln, z.B. Hauptseite anzeigen
+            else:
+                st.error("Falsches Passwort")
+        else:
+            st.error("Benutzername existiert nicht. Bitte registriere dich zuerst.")
+
     elif st.button("Registrieren"):
         users = sh.col_values(1)
         if username in users:
